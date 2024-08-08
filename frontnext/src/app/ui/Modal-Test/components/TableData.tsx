5f0cb901b29dea71d03d02ab9f556a69a9a1e4b7
이연승 <leeys951006@gmail.com>
2024-08-08 17:36:08 +0900

    :memo: 이연승 : 열이름 수정 구현

--- frontnext/src/app/ui/Modal-Test/components/TableData.tsx
+++ frontnext/src/app/ui/Modal-Test/components/TableData.tsx
@@ -3,20 +3,23 @@ import React, { useState, useEffect } from 'react';
 interface TableDataProps {
   data: any[];
   onSave: (updatedData: any[]) => void;
+  onColumnSave: (updatedColumns: string[]) => void;
 }
 
-const TableData: React.FC<TableDataProps> = ({ data, onSave }) => {
+const TableData: React.FC<TableDataProps> = ({ data, onSave, onColumnSave }) => {
   const [editableData, setEditableData] = useState(data);
+  const [editableColumns, setEditableColumns] = useState<string[]>([]);
 
   useEffect(() => {
     setEditableData(data);
+    if (data.length > 0) {
+      setEditableColumns(Object.keys(data[0]));
+    }
   }, [data]);
 
   if (editableData.length === 0) {
     return <div>데이터가 없습니다.</div>;
   }
-
-  const columns = Object.keys(editableData[0]);
 
   const handleChange = (rowIndex: number, column: string, value: string) => {
     const newData = editableData.map((row, index) => {
@@ -28,8 +31,18 @@ const TableData: React.FC<TableDataProps> = ({ data, onSave }) => {
     setEditableData(newData);
   };
 
+  const handleColumnChange = (colIndex: number, value: string) => {
+    const newColumns = [...editableColumns];
+    newColumns[colIndex] = value;
+    setEditableColumns(newColumns);
+  };
+
   const handleSave = () => {
     onSave(editableData);
+  };
+
+  const handleColumnSave = () => {
+    onColumnSave(editableColumns);
   };
 
   return (
@@ -37,12 +50,17 @@ const TableData: React.FC<TableDataProps> = ({ data, onSave }) => {
       <table className="min-w-full bg-white border border-gray-200">
         <thead>
           <tr>
-            {columns.map((column, index) => (
+            {editableColumns.map((column, index) => (
               <th
                 key={index}
                 className="py-2 px-4 border-b border-gray-200 bg-gray-100 text-left"
               >
-                {column}
+                <input
+                  type="text"
+                  value={column}
+                  onChange={(e) => handleColumnChange(index, e.target.value)}
+                  className="w-full px-2 py-1 border rounded"
+                />
               </th>
             ))}
           </tr>
@@ -50,7 +68,7 @@ const TableData: React.FC<TableDataProps> = ({ data, onSave }) => {
         <tbody>
           {editableData.map((row, rowIndex) => (
             <tr key={rowIndex} className="hover:bg-gray-100">
-              {columns.map((column, colIndex) => (
+              {editableColumns.map((column, colIndex) => (
                 <td
                   key={colIndex}
                   className="py-2 px-4 border-b border-gray-200"
@@ -72,6 +90,12 @@ const TableData: React.FC<TableDataProps> = ({ data, onSave }) => {
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
       >
         저장
+      </button>
+      <button
+        onClick={handleColumnSave}
+        className="mt-4 ml-2 px-4 py-2 bg-green-500 text-white rounded"
+      >
+        열 저장
       </button>
     </div>
   );
